666fe5564066d3337031053439b05ee40ab004f3
pamuki <104628715+pamuki@users.noreply.github.com>
2023-11-05 14:44:11 +0100

    Demo_Final

--- Blahamak/Assets/FPPM/Szkriptek/pauseGame.cs
+++ Blahamak/Assets/FPPM/Szkriptek/pauseGame.cs
@@ -13,7 +13,7 @@ public class pausescript : MonoBehaviour
     }
 
     void Update(){
-        if(Input.GetKeyDown(KeyCode.Escape)){
+        if(Input.GetKeyDown(KeyCode.P)){
             if(isPaused){
                 ResumeGame();
             }
